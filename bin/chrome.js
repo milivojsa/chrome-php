3ecd7539a71816ff24e883f7a823c365631de05d
Torgheh <mn2hh5@gmail.com>
2019-04-18 23:15:26 +0200

    Update chrome.js
    Change user agent from chromeheadless to chrome.

--- bin/chrome.js
+++ bin/chrome.js
@@ -5,12 +5,15 @@ const options = JSON.parse(process.argv[2]);
 const runChrome = async () => {
         let browser;
         let page;
-
+        var user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36';
         try {
             browser = await puppeteer.launch({
                 ignoreHTTPSErrors: true,
                 executablePath: options.path,
-                args: ['--no-sandbox']
+                args: [
+                    '--no-sandbox',
+                    '--user-agent='+user_agent
+                ]
             });
 
             page = await browser.newPage();
